19eeebad00220bd4b0ff4a1724abed304fc4533b
balintp105 <balintp105@gmail.com>
2024-09-22 17:52:32 +0200

    errors fixed

--- main.cpp
+++ main.cpp
@@ -1,26 +1,41 @@
-#include <iostream>
+  #include <iostream>
 
 constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // hiba1
-    std::cout << '1-100 ertekek duplazasa' // hiba2
-    for (int i = 0;) // hiba3
+    //int *b = new int[NELEMENTS]; // hiba1
+    int* b = new int[N_ELEMENTS];
+
+    //std::cout << '1-100 ertekek duplazasa' // hiba2
+    std::cout << "1-100 ertekek duplazasa";
+
+    // for (int i = 0;) // hiba3
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2;
+        b[i] = (i+1) * 2;
     }
-    for (int i = 0; i; i++) // hiba4
+
+    // for (int i = 0; i; i++) // hiba4
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // hiba5
-    }    
+        //std::cout << "Ertek:" // hiba5
+        std::cout << "Ertek:" << b[i]<<"\n";
+    }
+
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // hiba6
-    for (int i = 0; i < N_ELEMENTS, i++) // hiba7
+
+    //int atlag; // hiba6
+    int atlag=0;
+
+    //for (int i = 0; i < N_ELEMENTS, i++) // hiba7
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // hiba8
+        //atlag += b[i] // hiba8
+        atlag += b[i];
     }
-    atlag /= N_ELEMENTS; 
+
+    atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
